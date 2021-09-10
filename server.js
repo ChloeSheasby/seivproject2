02020de94bae8942a10f2d2572f4e446b56bf9e0
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2021-09-10 10:36:01 -0500

    Minor changes to node connection

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
+
+var corsOptions = {
+  origin: 'http://localhost:8080',
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors());
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
